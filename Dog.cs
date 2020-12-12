109cd47883f0c883a462544f46b45de77cde6db0
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-12-12 10:54:28 +0200

    init

---
+++ Dog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1.OOPExample
+{
+    public class Dog : Mammal // class corresponds with the fileName!
+    {
+        
+        public string Breed { get; set; }
+        public Dog(int age, string breed) : base(age)   // constructor inherits from base class constructor so we call base class constructor!
+        {
+            this.Breed = breed;
+        }
+
+
+        public void WagTail() 
+        {
+            Console.WriteLine("Tail waggling ...");
+        }
+         // program does not have ab ebtry point, a method main it it !!
+    }
+}
---
+++ Mammal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1.OOPExample
+{
+    public class Mammal
+    {
+        public int Age { get; set; }
+        public Mammal(int age) 
+        {
+            this.Age = age;
+        }
+        public void Sleep() 
+        {
+            Console.WriteLine("shhtt , i am sleepuing!");
+          
+        }
+         // program does not contain an entry pioint !
+        
+    }
+}
